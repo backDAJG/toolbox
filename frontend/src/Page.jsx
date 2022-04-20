6c53d2b6d3fb820b892836c021965ff0eb99ed28
Alejandro Jerez <jerez1.2.3.102107@gmail.com>
2022-04-20 15:38:52 -0500

    chore(add: backend for frontend)

--- frontend/src/Page.jsx
+++ frontend/src/Page.jsx
@@ -1,8 +1,23 @@
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
+import axios from 'axios';
 import Navbar from './components/Navbar';
 import Table from './components/Table';
+import { useDispatch } from 'react-redux';
+import { SET_FILES } from './store/actions';
 
 export default () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    axios({
+      url: 'http://localhost:3001/files/data',
+      method: 'GET',
+    })
+      .then((res) => {
+        dispatch({ type: SET_FILES, data: res.data.files });
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <Fragment>
       <Navbar />
--- frontend/src/components/Table.jsx
+++ frontend/src/components/Table.jsx
@@ -1,6 +1,8 @@
 import { Table, Container } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
 
 export default () => {
+  const { files } = useSelector((state) => state.files);
   return (
     <Container>
       <Table style={{ marginTop: 30 }} striped bordered hover variant="dark">
@@ -12,6 +14,20 @@ export default () => {
             <th>Hex</th>
           </tr>
         </thead>
+        <tbody>
+          {files.map((file) => {
+            return file.lines.map((line, index) => {
+              return (
+                <tr key={index}>
+                  <td>{file.file}</td>
+                  <td>{line.text}</td>
+                  <td>{line.number}</td>
+                  <td>{line.hex}</td>
+                </tr>
+              );
+            });
+          })}
+        </tbody>
       </Table>
     </Container>
   );
